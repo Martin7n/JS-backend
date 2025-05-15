0018e09b0179a8f5562020ee090dc45a6107bf79
Martin7n <122726440+Martin7n@users.noreply.github.com>
2025-05-15 15:04:31 +0300

    first run...

--- cat-shelter/index.js
+++ cat-shelter/index.js
@@ -1,10 +1,13 @@
 import http, { createServer } from "http";
+import { indexContent } from "./resources/views/index.html.js";
 
+
+console.log(indexContent)
 
 const server = createServer(function(req, res) {
 
     console.log("new RQ")
-    res.write("<h1>Pretty nice Hello request received into h1 output...</h1>")
+    res.write(indexContent)
     res.end();
 
 });
