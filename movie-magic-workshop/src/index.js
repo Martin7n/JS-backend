6f5745b01ce0d3e5b2c47fb5a8e810278847c299
Martin7n <122726440+Martin7n@users.noreply.github.com>
2025-05-22 12:08:24 +0300

    resources upd

--- movie-magic-workshop/src/index.js
+++ movie-magic-workshop/src/index.js
@@ -1,3 +1,8 @@
+import express from 'express';
+import handlebars from 'express-handlebars'
 
 
-console.log("init, setup and etc")
+const app = express();
+app.use(express.static('./src/public'));
+app.set('view engine', 'hbs');
+app.set('views', './src/views');
