6e37ec0150563950e3449d2fb233d8e7a6e41396
Martin7n <122726440+Martin7n@users.noreply.github.com>
2025-05-22 12:56:13 +0300

    templates setup

--- movie-magic-workshop/src/index.js
+++ movie-magic-workshop/src/index.js
@@ -4,11 +4,13 @@ import handlebars from 'express-handlebars'
 
 const app = express();
 app.use(express.static('./src/public'));
+app.use(express.urlencoded());
 app.engine('hbs', handlebars.engine({
     extname: 'hbs',
 }));
 app.set('view engine', 'hbs');
 app.set('views', './src/views');
+
 
 // // test1
 // app.get('/', (req, res) => {
@@ -16,9 +18,13 @@ app.set('views', './src/views');
 // }); 
 
 
+const context = {
+    name: "contextTestNameValue"
+}
+
 app.get('/', (req, res) => {
     // res.send('<h1>It works!</h1>'); 
-    res.render('home', { layout: false });
+    res.render('home', {context});
 });
 
 
