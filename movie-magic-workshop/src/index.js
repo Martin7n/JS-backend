e5dab80d5553e849aba721b8f9c016dc4e5a2d9a
Martin7n <122726440+Martin7n@users.noreply.github.com>
2025-05-22 12:22:30 +0300

    hbs setup

--- movie-magic-workshop/src/index.js
+++ movie-magic-workshop/src/index.js
@@ -4,5 +4,25 @@ import handlebars from 'express-handlebars'
 
 const app = express();
 app.use(express.static('./src/public'));
+app.engine('hbs', handlebars.engine({
+    extname: 'hbs',
+}));
 app.set('view engine', 'hbs');
 app.set('views', './src/views');
+
+// // test1
+// app.get('/', (req, res) => {
+//     res.send('It works!'); 
+// }); 
+
+
+app.get('/', (req, res) => {
+    // res.send('<h1>It works!</h1>'); 
+    res.render('home', { layout: false });
+});
+
+
+
+
+
+app.listen(5001, () => console.log('Server is listening on http://localhost:5001....'));
